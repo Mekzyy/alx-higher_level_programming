0f3b17be93d66c5eb9143d0834f601ef8b74affe
Mekzyy <emeka.pesos@icloud.com>
2022-12-08 04:57:12 +0000

    task 12

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
-def complex_delete(a_dictionary, value):
-        new_dict = dict(a_dictionary)
-        for k, v in new_dict.items():
-            if v == value:
-                del a_dictionary[k]
-        return a_dictionary
+def complex_delete(my_dict, value):
+    copy = my_dict.copy()
+    for k, v in copy.items():
+        if value in v:
+            del my_dict[k]
+    return my_dict
